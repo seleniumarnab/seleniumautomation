b828b45932d6c3a5c1a106d299e15f4cd16ce494
arnab.m <arnab.m@GSL-KOL-DT-11.GSL.Kolkata>
2019-04-23 16:36:03 +0530

    commit from ofc 1

--- smp/src/main/java/Utility/RetryAnalyzer.java
+++ smp/src/main/java/Utility/RetryAnalyzer.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
 	int count = 0;
-	int limit = 4;
+	int limit = 5;
 
 	public boolean retry(ITestResult result) {
 
