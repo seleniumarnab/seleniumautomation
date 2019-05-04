b5866283edcd1b0ff33b0bf2e3f5b0a20ffddfb2
seleniumarnab <arnabmukherjee002@gmail.com>
2019-05-04 18:53:04 +0530

    From home string rev updated.

--- smp/src/main/java/Utility/RetryAnalyzer.java
+++ smp/src/main/java/Utility/RetryAnalyzer.java
@@ -8,7 +8,7 @@ public class RetryAnalyzer implements IRetryAnalyzer {
 	
 	//update from office
 	int count = 0;
-	int limit = 11;
+	int limit = 3;
 
 	public boolean retry(ITestResult result) {
 
--- smp/src/main/java/Utility/StringReverse.java
+++ smp/src/main/java/Utility/StringReverse.java
@@ -2,18 +2,29 @@ package Utility;
 
 public class StringReverse {
 
+	
 	public static void main(String[] args) {
 		
 		String s="String";
 	    char[] c= s.toCharArray();
-	    int a= c.length;
-		//System.out.println(a);
-	
-		for (int i=a-1;i>=0;i--) {
+	    
+	    
+	    //string reverse
+	    for (int i=c.length-1;i>=0;i--) {
 			
-			System.out.println(c[i]);
+			System.out.println("Reverse of string is:" +c[i]);
 		}
-		
+	    
+	   
+	   //string length
+       System.out.println("\nString length is" +c.length+"\n");
+	    
+	    
+       //string to character array print
+	    for (int i=0;i<=c.length-1;i++) {
+			
+			System.out.println("String to character" +c[i]);
+		}
 
 	}
 
